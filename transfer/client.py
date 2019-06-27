d08ce544d04c55070ae06a27fdbe1dbf98dbacb7
Dohyeon An <gomsoup@gmail.com>
2019-06-27 08:35:01 -0700

    server-client done

--- transfer/client.py
+++ transfer/client.py
@@ -7,60 +7,51 @@ PORT = 50000
 ADDR = (HOST, PORT)
 BUFF_SIZE = 1024
 
-filenames = os.listdir("./pic")
-filenames = [file for file in filenames if file.endswith(".bmp")]
+filenames = os.listdir("/etc/face/pic")
+filenames = [file for file in filenames if file.endswith(".png")]
 
 stdnum = "201324474"
 zip = zipfile.ZipFile(stdnum + ".zip", "w")
 
+print ("zip start")
 for file in filenames:
-    zip.write(os.path.join("./pic", file), file, compress_type = zipfile.ZIP_DEFLATED)
+    print (file)
+    zip.write(os.path.join("/etc/face/pic", file), file, compress_type = zipfile.ZIP_DEFLATED)
 zip.close()
+print ("zip ended")
 
 
 clientSocket = socket.socket()
 clientSocket.connect(ADDR)
 
-clientSocket.send("201324474".encode())
+clientSocket.send(stdnum.encode())
+clientSocket.recv(1024)
+
+filesize = os.path.getsize(stdnum + '.zip')
+clientSocket.send(str(filesize))
 clientSocket.recv(1024)
 
 with open(stdnum + ".zip", 'rb') as f:
     print ("file opened")
 
-    print ("sending data...")
+    cnt = 0
+    
     l = f.read(4096)
-
     while(l):
         clientSocket.send(l)
         l = f.read(4096)
-    f.close()
-
+     
+    clientSocket.shutdown(socket.SHUT_WR)    
     print("Successfully send the file")
 
+    print("waiting server yml...")
+    fd = open("/etc/face/face.yml".encode(), 'wb')
+    while True:
+        data = clientSocket.recv(4096)
+        if not data:
+            break
+        fd.write(data)    
+    fd.close()
+
 clientSocket.close()
-
-"""
-for path in filenames:
-    clientSocket = socket.socket()
-    clientSocket.connect(ADDR)
-    full_filename = os.path.join("./pic", path)
-    print (path)
-    clientSocket.send("201324474".encode())
-    clientSocket.recv(1024)
-    clientSocket.send((path).encode("UTF-8"))
-    clientSocket.recv(1024)
-
-    with open(full_filename.encode(), 'rb') as f:
-        print ('file opened')
-        
-        print('sending data...')
-        l = f.read(4096)
-        while (l):
-            clientSocket.send(l)
-            l = f.read(4096)
-    f.close()
-
-    print('Successfully send the file')
-    clientSocket.close()
-"""
 print('connection closed')
--- transfer/server.py
+++ transfer/server.py
@@ -5,28 +5,6 @@ import zipfile
 import numpy
 from PIL import Image
 
-def getImagesWithID(path):
-    faces = []
-    labels = []
-
-    filenames = os.listdir(path)
-    filenames = [file for file in filenames if file.endswith(".png")]
-
-    for file in filenames:
-        print(file)
-        img = Image.open(path + file).convert('L')
-
-        img_numpy = numpy.array(img, 'uint8')
-
-        face = faceDetect(img_numpy)
-
-        if face is not None:
-            face = cv2.resize(face, (200,200))
-            faces.append(face)
-            labels.append(stdnum)
-        
-    return faces, labels
-
 def faceDetect(img):
     cas = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
     faces = cas.detectMultiScale(img, 1.3, 5)
@@ -41,6 +19,32 @@ def faceDetect(img):
         cropped_face = img[y:y+h, x:x+w]
     #cropped_face 리턴 
     return cropped_face
+
+def getImagesWithID(path):
+    faces = []
+    labels = []
+
+    filenames = os.listdir(path)
+    filenames = [file for file in filenames if file.endswith(".png")]
+
+    for file in filenames:
+        print (file)
+        img = Image.open(path + file).convert('L')
+
+        img_numpy = numpy.array(img, 'uint8')
+
+        face = faceDetect(img_numpy)
+        if face is not None:
+            face = cv2.resize(face, (200,200))
+            faces.append(face)
+            labels.append(stdnum)
+        else:
+            continue
+    
+    print (faces)
+    print (labels)
+    return faces, labels
+
 
 HOST = socket.gethostname()
 PORT = 50000
@@ -62,6 +66,10 @@ while True:
     print('client : ' + stdnum.decode())
     clientSocket.send("ok".encode())
 
+    size = int(clientSocket.recv(1024))
+    print('filesize : ' + str(size))
+    clientSocket.send('ok'.encode())
+
     try:
         if not(os.path.isdir(stdnum)):
             os.makedirs(os.path.join(stdnum))
@@ -74,18 +82,18 @@ while True:
     print("filename : " + path)
 
     with open(path.encode(), 'wb') as f:
+        
             print ('file opened')
             print ('receiving data...')
+        
             while True:
                 data = clientSocket.recv(4096)
                 if not data:
                     break
                 f.write(data)
 
-    f.close()
+            f.close()
     print('Successfully get the file')
-    clientSocket.close()
-    print('connection closed')
 
     print('unzip start')
     zip = zipfile.ZipFile(stdnum.decode() + "\\" + stdnum.decode() + ".zip")
@@ -93,19 +101,28 @@ while True:
     zip.close()
     print('unzip end') 
 
-    stdnum = stdnum.decode()   
- 
+    stdnum = int(stdnum.decode())
+  
 #if __name__ == "__main__":
-    stdnum = 201324474
-    
-
+    print("crop faces...")
     path = str(stdnum) + "\\" # + "faces" "\\"
     recognizer = cv2.face.LBPHFaceRecognizer_create()
 
     faces, labels = getImagesWithID(path)
 
-    print (faces)
-    print (labels)
-
+    print("now training..")
     recognizer.train(faces, numpy.array(labels))
     recognizer.save("./train.yml")
+
+    print("training done. send yml...")
+    fd = open("./train.yml", "rb")
+
+    data = fd.read(4096)
+    while (data):
+        clientSocket.send(data)
+        data = fd.read(4096)
+    fd.close()
+    print ("yml sended")
+
+    clientSocket.close()
+    print('connection closed')
